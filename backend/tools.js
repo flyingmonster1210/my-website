c98eba57899b7c7b054f03c9023c04c5b1821887
Wayne <zweijie1210@outlook.com>
2023-09-15 17:08:55 -0700

    Upload isEmpty( )

--- backend/tools.js
+++ backend/tools.js
@@ -1,19 +1,38 @@
 const isEmpty = (obj, keys) => {
+  if (obj === null || obj === undefined) {
+    return {
+      result: true,
+      message: 'The obj is null or undefined.'
+    }
+  }
+
   if (Object.keys(obj).length === 0) {
-    return true
+    return {
+      result: true,
+      message: 'The obj is empty.'
+    }
   }
 
   if (keys.length === 0) {
-    return false
+    return {
+      result: false,
+      message: 'The keys array is empty.'
+    }
   }
 
   for (let i = 0; i < keys.length; i++) {
     if (!(keys[i] in obj)) {
-      return true
+      return {
+        result: true,
+        message: 'The obj does not contain the require field(s).'
+      }
     }
   }
 
-  return false
+  return {
+    result: false,
+    message: 'The obj is not empty and it contains the required field(s).'
+  }
 }
 
 module.exports = {

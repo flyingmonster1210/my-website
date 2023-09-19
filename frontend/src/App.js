037a7ffe6975c419a742ca08301eb6812e0a9cfd
Wayne <zweijie1210@outlook.com>
2023-09-19 16:29:13 -0700

    Routers are set in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
 import Footer from './components/Footer'
-import LandingPage from './pages/LandingPage/LandingPage'
+import LandingPage from './pages/landingPage/LandingPage'
+import ProjectsPage from './pages/projectsPage/ProjectsPage'
+import UserPage from './pages/userPage/UserPage'
 
 function App () {
   return (
-    <div className='flex flex-col justify-between min-h-screen overflow-hidden bg-yellow-50'>
-      <Navbar />
-      <LandingPage />
-      <Footer />
-    </div>
+    <>
+      <Router>
+        <div className='flex flex-col justify-between min-h-screen overflow-hidden bg-yellow-50'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/projects' element={<ProjectsPage />} />
+            <Route path='/user' element={<UserPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   )
 }
 

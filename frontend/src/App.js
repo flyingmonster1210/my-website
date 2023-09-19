02c3f865cbf126fb8d4389dc518e1e545f4be2f2
Wayne <zweijie1210@outlook.com>
2023-09-19 16:02:27 -0700

    Navbar initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import LandingPage from './pages/LandingPage/LandingPage'
 
 function App () {
   return (
     <div className='flex flex-col justify-between min-h-screen overflow-hidden bg-yellow-50'>
+      <Navbar />
       <LandingPage />
       <Footer />
     </div>

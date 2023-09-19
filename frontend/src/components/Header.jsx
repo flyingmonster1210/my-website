05b08efe8c5740f242adeb0ebc2789a81423f24c
Wayne <zweijie1210@outlook.com>
2023-09-19 16:17:17 -0700

    Change Navbar to Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div
+      id="header"
+      className="flex flex-row justify-between items-center bg-yellow-50 px-12 py-3 font-poppins text-[15px] font-semibold"
+    >
+      <button>Home</button>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header

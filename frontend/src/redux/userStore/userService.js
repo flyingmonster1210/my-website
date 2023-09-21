6d1fec7c06ec5ba81fb2f022cbf7a12a038ed864
Wayne <zweijie1210@outlook.com>
2023-09-21 15:58:32 -0700

    Extract user object from response.data

--- frontend/src/redux/userStore/userService.js
+++ frontend/src/redux/userStore/userService.js
@@ -1,13 +1,74 @@
 import axios from 'axios'
 
 const API_URL = '/api/user/'
+const LOCAL_STORAGE_KEY = 'user'
 
-const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+// User register
+const register = async (userData) => {
+  const { data } = await axios.post(API_URL + 'register', userData)
 
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+  if (data && data.user) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data.user))
   }
 
-  return response.data
+  return (data && data.user) ? data.user : data
 }
+
+// User login
+const login = async (userData) => {
+  const { data } = await axios.post(API_URL + 'login/', userData)
+
+  if (data && data.user) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data.user))
+  }
+
+  return (data && data.user) ? data.user : data
+}
+
+// User logout
+// TODO: After logout, set the default user info into loaclStorage
+const logout = () => {
+  localStorage.removeItem(LOCAL_STORAGE_KEY)
+}
+
+// User update profile
+const update = async (userId, userData) => {
+  const { data } = await axios.put(API_URL + 'update/' + userId, userData)
+
+  if (data && data.user) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data.user))
+  }
+
+  return (data && data.user) ? data.user : data
+}
+
+// get and set the default user info into the localStorage
+const loadAndSetDefaultUserInfo = async () => {
+  const { data } = await axios.get(API_URL + 'me/')
+
+  if (data && data.user) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data.user))
+  }
+
+  return (data && data.user) ? data.user : data
+}
+
+// Get user profile by id
+const getMe = async () => {
+  // TODO: Handle the request with id
+
+  const { data } = await axios.get(API_URL + 'me/')
+
+  return (data && data.user) ? data.user : data
+}
+
+const userService = {
+  register,
+  login,
+  logout,
+  update,
+  getMe,
+  loadAndSetDefaultUserInfo,
+}
+
+export default userService

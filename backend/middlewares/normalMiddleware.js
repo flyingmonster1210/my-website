de816135866f54a97f6e5ac0cf74505be6a68013
Wayne <zweijie1210@outlook.com>
2023-09-21 16:36:44 -0700

    Normal middleware will show the full url

--- backend/middlewares/normalMiddleware.js
+++ backend/middlewares/normalMiddleware.js
@@ -1,6 +1,8 @@
 const requestInfo = (req, res, next) => {
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
   console.log('\n* ' + (new Date()).toString())
   console.log(`  Request method: ${req.method}`)
+  console.log('  Full URL: ' + fullUrl.cyan.underline)
   next()
 }
 

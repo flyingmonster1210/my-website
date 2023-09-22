6a1577bad45107dc2f88feb5b310f3f3eb2417d8
Wayne <zweijie1210@outlook.com>
2023-09-22 14:50:57 -0700

    Component Alert is done

---
+++ frontend/src/components/Alert.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export const Alert = (props) => {
+  const { type, title, message } = props
+  let color = {
+    bg: 'bg-teal-100',
+    border: 'border-teal-500',
+    text: 'text-teal-900',
+  }
+  if (type === 'rejected') {
+    color = {
+      bg: 'bg-red-100',
+      border: 'border-red-400',
+      text: 'text-red-700',
+    }
+  }
+
+  return (
+    <div className="static">
+      <div
+        className={`absolute top-2 right-0 border-t-4 rounded-b px-4 py-3 shadow-md ${color.bg} ${color.border} ${color.text}`}
+        role="alert"
+      >
+        <div className="flex">
+          {/* <div className="py-1">
+            <svg
+              className={`fill-current h-6 w-6 mr-4 ${color.text}`}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+            </svg>
+          </div> */}
+          <div>
+            <p className="font-bold">{props.title}</p>
+            <p className="text-sm">{props.message}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

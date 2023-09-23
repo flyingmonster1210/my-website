e0f6cddd41f4e93995af4c0ac84779a4ccd53781
Wayne <zweijie1210@outlook.com>
2023-09-23 00:46:15 -0700

    Update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,40 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { logout } from '../redux/userStore/userSlice'
 
 function Header() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
   const url = useLocation().pathname
   const { user } = useSelector((state) => state.user)
-  const { token } = user
+  const token = user ? user.token : user
+
+  const loginBtn = (
+    <button
+      className="hover:text-gray-500"
+      onClick={() => {
+        navigate('/login')
+      }}
+    >
+      Login
+    </button>
+  )
+  const logoutBtn = (
+    <button
+      className="hover:text-gray-500"
+      onClick={async () => {
+        try {
+          await dispatch(logout()).unwrap()
+          navigate('/')
+        } catch (error) {
+          console.log(error)
+        }
+      }}
+    >
+      Logout
+    </button>
+  )
 
   return (
     <div
@@ -20,16 +49,7 @@ function Header() {
       >
         Home
       </button>
-      {url.includes('user') ? null : (
-        <button
-          className="hover:text-gray-500"
-          onClick={() => {
-            navigate('/login')
-          }}
-        >
-          {token ? 'Logout' : 'Login'}
-        </button>
-      )}
+      {token ? logoutBtn : loginBtn}
     </div>
   )
 }

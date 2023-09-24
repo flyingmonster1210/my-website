71f176124a081e0e44fd8abfba9597136055a444
Wayne <zweijie1210@outlook.com>
2023-09-23 23:54:08 -0700

    Change the output style

--- backend/middlewares/normalMiddleware.js
+++ backend/middlewares/normalMiddleware.js
@@ -1,8 +1,8 @@
 const requestInfo = (req, res, next) => {
   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
-  console.log('\n* ' + (new Date()).toString())
-  console.log(`  Request method: ${req.method}`)
-  console.log('  Full URL: ' + fullUrl.cyan.underline)
+  console.log('\n* ' + (new Date()).toString().green)
+  console.log('  Request method: '.gray + req.method.white)
+  console.log('  Full URL: '.gray + fullUrl.cyan.underline)
   next()
 }
 

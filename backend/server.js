b81b80ba24e359cc8ec9286360caf3d8fb20e293
Wayne <zweijie1210@outlook.com>
2023-09-29 20:17:26 -0700

    Change logging info in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,9 @@ app.use('/api/project', require('./routes/projectRoutes'))
 app.use('/api/user', require('./routes/userRoutes'))
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('production')
-
   app.use(express.static(path.join(__dirname, '../frontend/build')))
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 } else {
-  console.log('development')
-
   app.get('/', (req, res) => res.send('Please set to production mode!'))
 }
 
